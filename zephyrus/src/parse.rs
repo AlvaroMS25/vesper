db0ebc2b9d09c6358acd5a61c1e3046268c292f3
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-07-09 19:58:06 +0200

    move `Parse` method order, change return types and add a method

--- zephyrus/src/parse.rs
+++ zephyrus/src/parse.rs
@@ -1,4 +1,4 @@
-use crate::{builder::WrappedClient, twilight_exports::*};
+use crate::{argument::ArgumentLimits, builder::WrappedClient, twilight_exports::*};
 use async_trait::async_trait;
 use std::error::Error;
 
@@ -24,12 +24,6 @@ impl std::error::Error for GenericParsingError {}
 /// The core trait of this framework, it is used to parse all command arguments
 #[async_trait]
 pub trait Parse<T: Send + Sync>: Sized {
-    /// Adds the possible choices to the argument, this function is usually implemented by the
-    /// derive macro, but can be overridden manually.
-    fn add_choices() -> Box<dyn Fn() -> Option<Vec<CommandOptionChoice>> + Send + Sync> {
-        Box::new(|| None)
-    }
-
     /// Parses the option into the argument.
     async fn parse(
         _http_client: &WrappedClient,
@@ -37,13 +31,23 @@ pub trait Parse<T: Send + Sync>: Sized {
         _value: Option<&CommandOptionValue>,
     ) -> Result<Self, ParseError>;
 
+    /// Returns the option type this argument has.
+    fn option_type() -> CommandOptionType;
+
     /// Sets if the argument is required, by default is true.
     fn is_required() -> bool {
         true
     }
 
-    /// Returns the option type this argument has.
-    fn option_type() -> CommandOptionType;
+    /// Adds the possible choices to the argument, this function is usually implemented by the
+    /// derive macro, but can be overridden manually.
+    fn add_choices() -> Option<Vec<CommandOptionChoice>> {
+        None
+    }
+
+    fn set_limits() -> Option<ArgumentLimits> {
+        None
+    }
 }
 
 /// The errors which can be returned from [Parse](self::Parse) [parse](self::Parse::parse) function.
