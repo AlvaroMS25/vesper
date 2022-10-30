aad65920f8e9cb8707066bc9cf333b676e7a17ed
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-10-30 18:11:03 +0100

    implement error_handler macro

--- zephyrus-macros/src/error_handler.rs
+++ zephyrus-macros/src/error_handler.rs
@@ -2,5 +2,47 @@ use proc_macro2::TokenStream as TokenStream2;
 use syn::{parse2, spanned::Spanned, Error, ItemFn, Result};
 
 pub fn error_handler(input: TokenStream2) -> Result<TokenStream2> {
-    todo!()
+    let fun = parse2::<ItemFn>(input)?;
+    let ItemFn {
+        attrs,
+        vis,
+        mut sig,
+        block,
+    } = fun;
+
+    match sig.inputs.len() {
+        c if c != 2 => {
+            // This hook is expected to have three arguments, a reference to an `SlashContext`,
+            // a &str indicating the name of the command and the result of a command execution.
+            return Err(Error::new(sig.inputs.span(), "Expected two arguments"));
+        }
+        _ => (),
+    };
+
+    // The name of the original function
+    let ident = sig.ident.clone();
+    // This is the name the given function will have after this macro's execution
+    let fn_ident = quote::format_ident!("_{}", &ident);
+    sig.ident = fn_ident.clone();
+
+    /*
+    Check the return of the function, returning if it does not match, this function is required
+    to return `()`
+    */
+    crate::util::check_return_type(&sig.output, quote::quote!(()))?;
+
+    let (_, ty) = crate::util::get_context_type_and_ident(&sig)?;
+    // Get the hook macro so we can fit the function into a normal fn pointer
+    let hook = crate::util::get_hook_macro();
+    let path = quote::quote!(::zephyrus::hook::ErrorHandlerHook);
+
+    Ok(quote::quote! {
+        pub fn #ident() -> #path<#ty> {
+            #path(#fn_ident)
+        }
+
+        #[#hook]
+        #(#attrs)*
+        #vis #sig #block
+    })
 }
