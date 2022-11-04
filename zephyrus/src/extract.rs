8914be57a11a35f4e92a4d3de358c2a147b937c2
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-11-04 14:47:05 +0100

    doc

--- zephyrus/src/extract.rs
+++ zephyrus/src/extract.rs
@@ -4,11 +4,18 @@ mod sealed {
     impl<T> Sealed for Option<T> {}
 }
 
+/// Defines what items are allowed to be returned from a command function. Since a command
+/// function must return a `Result<T, E>`, this trait is only implemented for that type.
 pub trait Returnable: sealed::Sealed {
     type Ok;
     type Err;
 }
 
+/// Used in the [`after hook`] to determine the inner item, which is required to implement the
+/// [returnable] trait.
+///
+/// [returnable]: self::Returnable
+/// [`after hook`]: crate::hook::AfterHook
 pub trait Optional: sealed::Sealed {
     type Inner;
 }
