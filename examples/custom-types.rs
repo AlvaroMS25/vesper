3b1afb72cb83eb2c19951a403f42a27489abf4c2
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-11-27 01:20:26 +0100

    add custom return types example

---
+++ examples/custom-types.rs
@@ -0,0 +1,116 @@
+use std::env;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use futures_util::StreamExt;
+use twilight_gateway::Cluster;
+use twilight_http::Client;
+use twilight_model::gateway::event::Event;
+use twilight_model::gateway::Intents;
+use twilight_model::http::interaction::{InteractionResponse, InteractionResponseData, InteractionResponseType};
+use twilight_model::id::Id;
+use zephyrus::framework::DefaultError;
+use zephyrus::prelude::*;
+
+// The framework accepts custom error types, however, the custom error must implement
+// `From<ParseError>`
+pub enum MyError {
+    Parse(ParseError),
+    Http(twilight_http::Error),
+    Other(DefaultError)
+}
+
+impl From<ParseError> for MyError {
+    fn from(value: ParseError) -> Self {
+        Self::Parse(value)
+    }
+}
+
+impl From<twilight_http::Error> for MyError {
+    fn from(value: twilight_http::Error) -> Self {
+        Self::Http(value)
+    }
+}
+
+impl From<DefaultError> for MyError {
+    fn from(value: DefaultError) -> Self {
+        Self::Other(value)
+    }
+}
+
+// Let's measure the time a command needs to respond to an interaction, to do this let's specify
+// a custom `Ok` return type for our commands.
+pub struct ElapsedTime(Duration);
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").unwrap();
+    let application_id = env::var("DISCORD_APPLICATION_ID").unwrap().parse::<u64>().unwrap();
+
+    let http_client = Arc::new(Client::new(token.clone()));
+
+    let (cluster, mut events) = Cluster::builder(token, Intents::empty())
+        .http_client(Arc::clone(&http_client))
+        .build()
+        .await
+        .unwrap();
+
+    cluster.up().await;
+
+    // The framework supports setting a custom return type for commands and some hooks, to specify
+    // them just pass them as generic types.
+    let framework = Framework::<_, ElapsedTime, MyError>::builder(http_client, Id::new(application_id), ())
+        .command(timer)
+        .after(after_hook)
+        .build();
+
+    while let Some((_, event)) = events.next().await {
+        match event {
+            Event::Ready(_) => {
+                framework.register_global_commands().await.unwrap();
+            },
+            Event::InteractionCreate(interaction) => framework.process(interaction.0).await,
+            _ => ()
+        }
+    }
+}
+
+#[after]
+async fn after_hook(
+    _: &SlashContext<()>,
+    command_name: &str,
+    result: Option<Result<ElapsedTime, MyError>>
+) {
+    // We don't have a custom error handler, so result will be always `Some`
+    let result = result.unwrap();
+
+    match result {
+        Ok(elapsed) => {
+            println!("Command {} took {} ms to execute", command_name, elapsed.0.as_millis())
+        },
+        Err(e) => match e {
+            MyError::Parse(p) => println!("An error occurred when parsing a command {}", p),
+            MyError::Http(e) => println!("An HTTP error occurred: {}", e),
+            MyError::Other(other) => println!("An error occurred {}", other)
+        }
+    };
+}
+
+#[command]
+#[description = "Measures the time needed to respond to an interaction"]
+async fn timer(ctx: &SlashContext<()>) -> Result<ElapsedTime, MyError> {
+    let start = Instant::now();
+
+    ctx.interaction_client.create_response(
+        ctx.interaction.id,
+        &ctx.interaction.token,
+        &InteractionResponse {
+            kind: InteractionResponseType::ChannelMessageWithSource,
+            data: Some(InteractionResponseData {
+                content: Some(String::from("")),
+                ..Default::default()
+            })
+        }
+    ).await?;
+
+    Ok(ElapsedTime(Instant::now() - start))
+}
