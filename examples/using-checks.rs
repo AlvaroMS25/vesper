9e31a29349dcfe89819d5d9d4daa3db45f58d5b3
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-11-28 12:24:10 +0100

    crate checks example

---
+++ examples/using-checks.rs
@@ -0,0 +1,76 @@
+use std::env;
+use std::sync::Arc;
+use futures_util::StreamExt;
+use twilight_gateway::Cluster;
+use twilight_http::Client;
+use twilight_model::gateway::event::Event;
+use twilight_model::gateway::Intents;
+use twilight_model::http::interaction::{InteractionResponse, InteractionResponseData, InteractionResponseType};
+use twilight_model::id::Id;
+use zephyrus::framework::DefaultError;
+use zephyrus::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").unwrap();
+    let application_id = env::var("DISCORD_APPLICATION_ID").unwrap().parse::<u64>().unwrap();
+
+    let http_client = Arc::new(Client::new(token.clone()));
+
+    let (cluster, mut events) = Cluster::builder(token, Intents::empty())
+        .http_client(Arc::clone(&http_client))
+        .build()
+        .await
+        .unwrap();
+
+    cluster.up().await;
+
+    let framework = Framework::builder(http_client, Id::new(application_id), ())
+        .command(hello)
+        .build();
+
+    while let Some((_, event)) = events.next().await {
+        match event {
+            Event::Ready(_) => {
+                // We have to register the commands for them to show in discord.
+                framework.register_global_commands().await.unwrap();
+            },
+            Event::InteractionCreate(interaction) => framework.process(interaction.0).await,
+            _ => ()
+        }
+    }
+}
+
+#[check]
+async fn only_guilds(ctx: &SlashContext<()>) -> Result<bool, DefaultError> {
+    // Only pass the check if the command has been executed inside a guild
+    Ok(ctx.interaction.guild_id.is_some())
+}
+
+#[check]
+async fn other_check(_ctx: &SlashContext<()>) -> Result<bool, DefaultError> {
+    Ok(true)
+}
+
+#[command]
+#[description = "Says hello"]
+// The `check` attribute accepts a comma separated list of checks, so we can add as many as we want.
+// If a single one returns `false`, the command won't execute. If a check has an error and the
+// command has a custom error handler, it will handle the error. Otherwise the `after` hook will
+// receive the error of the check.
+#[checks(only_guilds, other_check)]
+async fn hello(ctx: &SlashContext<()>) -> DefaultCommandResult {
+    ctx.interaction_client.create_response(
+        ctx.interaction.id,
+        &ctx.interaction.token,
+        &InteractionResponse {
+            kind: InteractionResponseType::ChannelMessageWithSource,
+            data: Some(InteractionResponseData {
+                content: Some(String::from("Hello!")),
+                ..Default::default()
+            })
+        }
+    ).await?;
+
+    Ok(())
+}
