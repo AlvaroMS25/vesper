6a1db3aea01b2f030dc184735b4945611b1d955e
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-02-08 10:13:53 +0100

    doc

--- zephyrus/src/modal.rs
+++ zephyrus/src/modal.rs
@@ -95,10 +95,8 @@ impl<'ctx, S> Future for WaitModal<'ctx, S> {
 
 /// Trait used to define modals that can be sent to discord and parsed by the framework.
 ///
-/// This trait is normally implemented using the [`Modal`] derive macro, refer to it to see full
+/// This trait is normally implemented using the derive macro, refer to it to see full
 /// documentation about its usage and attributes.
-///
-/// [`Modal`]: crate::macros::Modal
 pub trait Modal<D> {
     /// Creates the modal, returning the response needed to send it to discord.
     fn create(ctx: &SlashContext<'_, D>, custom_id: String) -> InteractionResponse;
