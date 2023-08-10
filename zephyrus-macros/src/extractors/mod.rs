837de866c28d6d2c857c4643bedf77bc2eeb5d6b
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-08-10 18:43:29 +0200

    export map content

--- zephyrus-macros/src/extractors/mod.rs
+++ zephyrus-macros/src/extractors/mod.rs
@@ -2,10 +2,12 @@ pub mod either;
 pub mod function_path;
 pub mod ident;
 pub mod list;
+pub mod map;
 
 pub use {
     either::*,
     function_path::*,
     ident::*,
-    list::*
+    list::*,
+    map::*
 };
