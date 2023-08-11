2a513de6dc5a7c4355b93f0251e2b03712bbe3ec
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-08-11 19:37:37 +0200

    create tuple & implement parse

--- zephyrus-macros/src/extractors/mod.rs
+++ zephyrus-macros/src/extractors/mod.rs
@@ -3,11 +3,13 @@ pub mod function_path;
 pub mod ident;
 pub mod list;
 pub mod map;
+pub mod tuple;
 
 pub use {
     either::*,
     function_path::*,
     ident::*,
     list::*,
-    map::*
+    map::*,
+    tuple::*
 };
---
+++ zephyrus-macros/src/extractors/tuple.rs
@@ -0,0 +1,60 @@
+use std::marker::PhantomData;
+
+use darling::{FromMeta, export::NestedMeta};
+use proc_macro2::Span;
+use quote::ToTokens;
+use syn::{Token, parse::{Parse, ParseStream}};
+
+pub struct Tuple2<K, V, D = Token![=]>(pub K, pub V, PhantomData<D>);
+
+impl<K, V, D> Tuple2<K, V, D> {
+    pub fn new(k: K, v: V) -> Self {
+        Self(k, v, PhantomData)
+    }
+
+    pub fn with_delimiter(k: K, v: V, delim: D) -> Self {
+        let _ = delim;
+        Self(k, v, PhantomData)
+    }
+}
+
+impl<K, V, D> Parse for Tuple2<K, V, D>
+where
+    K: Parse,
+    V: Parse,
+    D: Parse
+{
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let k = input.parse()?;
+        let _: D = input.parse()?;
+        let v = input.parse()?;
+
+        Ok(Self::new(k, v))
+    }
+}
+
+impl<K, V, D> FromMeta for Tuple2<K, V, D> 
+where
+    K: FromMeta,
+    V: FromMeta,
+{
+    fn from_list(items: &[NestedMeta]) -> darling::Result<Self> {
+        match items {
+            [a, b] => 
+                Ok(Self(K::from_nested_meta(a)?, V::from_nested_meta(b)?, PhantomData)),
+            _ => Err(darling::Error::unsupported_format("2 item tuple").with_span(&Span::call_site()))
+        }
+    }
+}
+
+impl<K, V, D> ToTokens for Tuple2<K, V, D>
+where
+    K: ToTokens,
+    V: ToTokens
+{
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let k = &self.0;
+        let v = &self.1;
+        tokens.extend(quote::quote!((#k, #v)))
+    }
+}
