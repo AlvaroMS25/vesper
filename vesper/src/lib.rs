05d37e845a2a7ea781154ba8816594c783fb88e1
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-02-02 09:51:35 +0100

    make module public

--- vesper/src/lib.rs
+++ vesper/src/lib.rs
@@ -20,7 +20,7 @@ pub mod wait;
 // Items used to extract generics from functions, not public API.
 #[doc(hidden)]
 pub mod extract;
-mod parsers;
+pub mod parsers;
 
 pub use vesper_macros as macros;
 
